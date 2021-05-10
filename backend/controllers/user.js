caa89030238b79a8e6eeb01588a1eee824df61a8
Sofiane <sa.dev672@gmail.com>
2021-05-10 10:30:50 +0200

    Implémentation de l'authentification

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,11 +1,19 @@
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
+/**
+ * Création d'un compte utilisateur avec cryptage du password
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ */
 exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
+    const { name, firstName, email, password } = req.body;
+    bcrypt.hash(password, 10)
     .then(hash => {
-        const { name, firstName, email} = req.body;
+        
         User.create({
             name,
             firstName,
@@ -15,4 +23,37 @@ exports.signup = (req, res, next) => {
         .then(() => res.status(201).json({message : "Utilisateur créé"}))
         .catch(error => res.status(500).json({error}))
     })
-}
+};
+
+/**
+ * Contrôle l'authentification et crée un token si l'identifiant et le mot de passe sont correct
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ */
+exports.login = (req, res, next) => {
+    const {email, password} = req.body;
+
+    User.findAll({where: {email: email}})
+    .then(([user]) => {
+        
+        if (!user) return res.status(401).json({ error: "L'adresse email n'existe pas" });
+
+        bcrypt.compare(password, user.password)
+            .then(valid => {
+                if (!valid) return res.status(401).json({ error: "Mot de passe incorrect" });
+
+                res.status(200).json({
+                    userId: user.id,
+                    token: jwt.sign(
+                        { userId: user.id },
+                        process.env.SECRET,
+                        { expiresIn: "24h" }
+                    )
+                });
+            })
+            .catch(error => res.status(500).json({ error }));
+
+    })
+    .catch(error => res.status(401).json({error}))
+};
