a2296d11e08691b8f09fbb4e8b0185fa0c8b0dfe
Sofiane <sa.dev672@gmail.com>
2021-06-25 21:45:24 +0200

    modification de la page signup

--- frontend/src/components/FormUser/FormUser.js
+++ frontend/src/components/FormUser/FormUser.js
@@ -1,72 +1,25 @@
-import Form from "../Form/Form";
 import FormGroup from "../FormGroup/FormGroup";
-import Button from "../UI/Button/Button";
 import Input from "../UI/Input/Input";
-import { useSelector, useDispatch} from "react-redux";
-import {useEffect, useState,Fragment} from "react";
+import {Fragment} from "react";
 import { formFieldUser } from "../../utils/formFields";
 import { GET_NAME, GET_FIRSTNAME, GET_EMAIL, GET_PASSWORD } from "../../redux/actions/form/type";
-import { showModal } from "../../redux/actions/modal/actions";
-import { signup } from "../../redux/actions/form/actions";
+
 
 const FormUser =  props => {
-    const dispatch = useDispatch();
-    const user = useSelector(store => store.user)
-    const isLoggedIn = useSelector(store => store.login.isLoggedIn);
-    const userDataStrored = useSelector(state => state.formInputValue);
-    const formIsSended = useSelector(state => state.formInputValue.isSend);
-    const [userData, setUserData] = useState();
-
-    const {request} = props;
-
-    useEffect(() => {
-        if (formIsSended) {
-            if(request == "signup" && !isLoggedIn){
-                dispatch(signup(userDataStrored))
-            }
-        }
-
-    }, [request, formIsSended])
-
-    const showDeleteModalHandler = e => {
-        dispatch(showModal({
-            isShow: true,
-            nameButton: "Supprimer",
-            type: "account",
-            userId: user.id,
-            sql: "delete",
-            content: "Confirmez vous la suppression de votre compte ?"
-        }))
-    };
-
     return (
         <Fragment>
-            {isLoggedIn && <h1>Mes informations</h1>}
-            <Form>
-                <FormGroup field={formFieldUser.name}>
-                    {!isLoggedIn ? <Input field={formFieldUser.name} action={GET_NAME} /> : <p>{user.name}</p>}
-                </FormGroup>
-                <FormGroup field={formFieldUser.firstName} >
-                    {!isLoggedIn ? <Input field={formFieldUser.firstName} action={GET_FIRSTNAME} /> : <p>{user.firstName}</p>}
-                </FormGroup>
-                <FormGroup field={formFieldUser.email}>
-                    {!isLoggedIn ? <Input field={formFieldUser.email} action={GET_EMAIL} /> : <p>{user.email}</p>}
-                </FormGroup>
-                {
-                    !isLoggedIn ? (
-                    <FormGroup field={formFieldUser.password}>
-                        <Input field={formFieldUser.password} action={GET_PASSWORD} />
-                    </FormGroup>
-                    ) : ""
-                }
-                
-                {!isLoggedIn ? <Button type="submit">Valider</Button> : (
-                    <div>
-                        <Button >Modifier mon profil</Button>
-                        <Button onClick={showDeleteModalHandler}>Supprimer mon compte</Button>
-                    </div>
-                    )}
-            </Form>
+            <FormGroup field={formFieldUser.name}>
+                <Input field={formFieldUser.name} action={GET_NAME} />
+            </FormGroup>
+            <FormGroup field={formFieldUser.firstName} >
+                <Input field={formFieldUser.firstName} action={GET_FIRSTNAME} />
+            </FormGroup>
+            <FormGroup field={formFieldUser.email}>
+                <Input field={formFieldUser.email} action={GET_EMAIL} />
+            </FormGroup>
+            <FormGroup field={formFieldUser.password}>
+                <Input field={formFieldUser.password} action={GET_PASSWORD} />
+            </FormGroup>
         </Fragment>
 
     )
--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -1,5 +1,37 @@
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import Form from "../components/Form/Form";
 import FormUser from "../components/FormUser/FormUser";
+import Button from "../components/UI/Button/Button";
+import { signup } from "../redux/actions/form/actions";
 
-const SignUp = () => <FormUser request="signup"/>
+const SignUp = () => {
+
+    const dispatch = useDispatch();
+    const formIsSended = useSelector(store => store.formInputValue.isSend);
+    const userDataStrored = useSelector(store => store.formInputValue);
+
+    useEffect(()=>{
+        if(formIsSended){
+
+            const user = {
+                name: userDataStrored.name,
+                firstName: userDataStrored.firstName,
+                email: userDataStrored.email,
+                password: userDataStrored.password
+            }
+
+            dispatch(signup(user));
+        }
+
+    },[formIsSended, userDataStrored, dispatch]); 
+
+    return(
+    <Form>
+        <FormUser/>
+        <Button type="submit">Valider</Button>
+    </Form>
+    )
+}
 
 export default SignUp;
